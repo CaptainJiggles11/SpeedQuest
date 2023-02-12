094886df1f5be4f2c1abcbae518f578be930fa67
CaptainJiggles11 <54753626+CaptainJiggles11@users.noreply.github.com>
2023-02-12 02:53:55 -0600

    Update PlayerCam.gd

--- scripts/PlayerCam.gd
+++ scripts/PlayerCam.gd
@@ -9,6 +9,5 @@ func _process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 	var player_pos = get_node("/root/Global").player_position
 	
-	#position = Vector2(player_pos.x - mouse_pos.x, player_pos.y - mouse_pos.y)
 	position = Vector2(player_pos.x,player_pos.y) + Vector2( mouse_pos.x - get_viewport_rect().size.x/2 , mouse_pos.y - get_viewport_rect().size.y/2)/2
-	pass
+	
