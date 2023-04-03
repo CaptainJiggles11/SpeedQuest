dcbf1583f189a392306464f798edf563809b481b
CaptainJiggles11 <54753626+CaptainJiggles11@users.noreply.github.com>
2023-04-03 14:13:20 -0500

    Monstro Fight

--- scenes/Slime_Sprite.gd
+++ scenes/Slime_Sprite.gd
@@ -4,32 +4,39 @@ var og_scale = scale
 var enter_anim = false
 var jump_end = Vector2.ZERO
 var power = 1
+var moving = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
+#try doing somethinghere
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	
+	if moving == true:
+		global_position = global_position.linear_interpolate(jump_end + Vector2(0,-50), .03)
+
 func squash(squash_scale, speed):
 	scale = squash_scale
 	var tween := create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "scale", og_scale, speed*power)
-		
+	
+func squeeze(squash_scale, speed):
+	scale = squash_scale
+	var tween := create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "scale", og_scale, speed*power)
+
 func up():
 	var tween := create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "global_position", global_position + Vector2(0,-45), 0.05)
 	squash(Vector2(.75,1.5), .5)
-	tween.tween_property(self, "global_position", global_position + Vector2(0,-50), 0.25)
 
+	
 func down():
 	var tweenn := create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
 	animation = "slime_fall"
 	tweenn.tween_property(self, "global_position", jump_end, 0.2)
+	moving = false
--- scripts/Slime_Boss.gd
+++ scripts/Slime_Boss.gd
@@ -20,9 +20,10 @@ var sfx
 var path: Array = []
 var level_navigation: Navigation2D = null
 export (PackedScene) var projectile
-var timer = 1
+var timer = 2
 var jump_dir = Vector2.ZERO
-
+var moving = false
+var collision
 
 onready var line2d = $Line2D
 
@@ -32,6 +33,7 @@ func _ready():
 	rb = $BossBody
 	sprite = $BossBody/Sprite
 	sfx = $BossBody/BossAudio
+	collision = $BossBody/CollisionShape2D
 	yield(get_tree(), "idle_frame")
 	
 	speed = 30
@@ -64,61 +66,54 @@ func _process(delta):
 				choose_attack()
 				
 		1: #Radial
-			if timer <= 0:
-				var shoot_angle = Vector2.UP
-				for x in range(128):
-					shoot(shoot_angle.normalized(),global_position + shoot_angle)
-					shoot_angle = shoot_angle.rotated(deg2rad(15/4))
-				randomize()
-				choose_attack()
-				timer = rand_range(2,5)
-			else:
-				timer -= delta
+			choose_attack()
 		2: #Burst
 			if timer <= 0:
-				var shoot_angle = Vector2.UP
-				for x in range(64):
-					var ran = rand_range(.75,1.5)
-					randomize()
-					var new_projectile = shoot((Global.player_position - global_position).normalized()*rand_range(0,1) + Vector2(rand_range(-.4,.4),rand_range(-.4,.4)) )
-					new_projectile.get_child(0).scale = Vector2(ran,ran)
-				randomize()
-				choose_attack()
-				timer = rand_range(3,6)
+				burst()
 			else:
 				timer -= delta
 		3: #Follow:
-			var rand = rand_range(.5,1)
-			if jumping == false:
-				var jump_start = global_position
-				var og_scale = sprite.scale
-				jump_dir = (Global.player_position - global_position).normalized()
-				sprite.power = rand
-				var jump_end = jump_dir * 100 * rand
-				sprite.jump_end = jump_end
-				sprite.animation = "slime_jump"
-				jumping = true
-				yield(sprite,"animation_finished")
-				sprite.scale = og_scale
-				sprite.animation = "slime_airborne"
-				sprite.up()
-				yield(get_tree().create_timer(rand), "timeout")
-				sprite.down()
-				yield(sprite,"animation_finished")
-				sprite.animation = "slime_stunned"
-				sprite.squash(Vector2(1.2,.8), .5)
-				yield(get_tree().create_timer(rand/1.5), "timeout")
-				jumping = false
-	
-			else:
-				if sprite.frame == 2 and sprite.animation == "slime_jump":
-					sprite.scale = Vector2(.8,1.2)
-				if sprite.frame == 0 and sprite.animation == "slime_fall":
-					sprite.scale = Vector2(.8,1.2)
-				global_position.linear_interpolate(sprite.jump_end,delta)
-				
-			
+			if timer <= 0:
+				var rand = rand_range(.5,1)
+				if jumping == false:
+					var jump_start = global_position
+					var og_scale = sprite.scale
+					jump_dir = (Global.player_position - global_position).normalized()
+					sprite.power = rand
+					var jump_end = Global.player_position
+					sprite.jump_end = jump_end
+					sprite.animation = "slime_jump"
+					jumping = true
+					collision.disabled = true
+					yield(sprite,"animation_finished")
+					moving = true
+					sprite.scale = og_scale
+					sprite.animation = "slime_airborne"
+					sprite.up()
+					yield(get_tree().create_timer(rand), "timeout")
+					sprite.down()
+					yield(sprite,"animation_finished")
+					radial()
+					sprite.animation = "slime_stunned"
+					sprite.squash(Vector2(1.2,.8), .5)
+					yield(get_tree().create_timer(rand/1.5), "timeout")
+					jumping = false
+					moving = false
+					collision.disabled = false
+					timer = rand
 
+	
+				else:
+					if sprite.frame == 2 and sprite.animation == "slime_jump":
+						sprite.scale = Vector2(.8,1.2)
+						sprite.moving = true
+					if sprite.frame == 0 and sprite.animation == "slime_fall":
+						sprite.scale = Vector2(.8,1.2)
+					if moving == true:
+						global_position = global_position.linear_interpolate(sprite.jump_end,.2)
+					
+			else:
+				timer -= delta
 
 func _on_RigidBody2D_body_shape_entered(body_id, body, body_shape, local_shape):
 	#print(body.name)
@@ -141,7 +136,7 @@ func generate_path():
 	if level_navigation != null:
 		path = level_navigation.get_simple_path(rb.global_position, Global.player_position, true)
 		line2d.points = path
-		
+
 func navigate(delta):
 	if path.size() > 0:
 		position += global_position.direction_to(path[1]).normalized() * actual_speed * delta
@@ -153,7 +148,7 @@ func death():
 	coin.position = position
 	get_parent().add_child(coin)
 	queue_free()
-	
+
 func shoot(direction = (Global.player_position - global_position).normalized(), shootPos = position + (Global.player_position - global_position).normalized() * 10):
 	var new_projectile = projectile.instance()
 	new_projectile.set("attack_damage", projectile_damage)
@@ -161,9 +156,35 @@ func shoot(direction = (Global.player_position - global_position).normalized(), 
 	new_projectile.set("start_pos", shootPos)
 	get_parent().add_child(new_projectile)
 	return new_projectile
-	
+
 func choose_attack():
 	my_attack = attack_type.values()[ randi()%attack_type.size() ]
-	
 
-	
+func burst():
+	timer = 100
+	sprite.animation = "slime_charge"
+	sprite.squeeze(Vector2(.9,1.5),.5)
+	yield(sprite,"animation_finished")
+	sprite.animation = "slime_shoot"
+	yield(sprite,"animation_finished")
+	sprite.animation = "slime_idle"
+	sprite.squash(Vector2(1.2,.8),.25)
+	var shoot_angle = Vector2.UP
+	for x in range(64):
+		var ran = rand_range(.75,1.5)
+		randomize()
+		var new_projectile = shoot((Global.player_position - global_position).normalized()*rand_range(0,1) + Vector2(rand_range(-.4,.4),rand_range(-.4,.4)) )
+		new_projectile.get_child(0).scale = Vector2(ran,ran)
+	randomize()
+	choose_attack()
+	timer = rand_range(2,3)
+
+func radial():
+	var shoot_angle = Vector2.UP
+	for x in range(128):
+		shoot(shoot_angle.normalized(),global_position + shoot_angle)
+		shoot_angle = shoot_angle.rotated(deg2rad(15/4))
+	randomize()
+	choose_attack()
+	timer = rand_range(3,5)
+
